8f95242943076bb3f074372804cbcef8a1f2ef88
leonardosoux <126433909+leonardosoux@users.noreply.github.com>
2024-03-20 00:45:05 -0300

    Atualizar o Ex-04.py

--- Exercicio-funcoes/Ex-04.py
+++ Exercicio-funcoes/Ex-04.py
@@ -23,10 +23,10 @@ def verificador(codigo):
     if len(codigo) != 7:
         return False
     
-    if codigo[0:1] != "BR" or codigo[6] != "X":
+    if codigo[0:2] != "BR" or codigo[6] != "X":
         return False
     
-    numero = int(codigo[2:5])
+    numero = int(codigo[2:6])
     if numero < 1 or numero > 9999:
         return False
     
