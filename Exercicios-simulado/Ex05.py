de5d1f1cc33abe0a3cb6baf19c42f18365a00780
Leonardo Souza <126433909+leonardosoux@users.noreply.github.com>
2024-03-25 23:39:05 -0300

    Atualizar o Ex05.py

--- Exercicios-simulado/Ex05.py
+++ Exercicios-simulado/Ex05.py
@@ -4,4 +4,9 @@ Solicite ao usuário que digite uma palavra ou frase e verifique se é um palín
 (ou seja, pode ser lida de frente para trás e de trás para frente da mesma forma).
 
 '''
+palavra = input("Digite uma palavra:")
+ if palavra == palavra[::-1]
+    print ("Esta palavra é um palindromo")
+else 
+    print("Esta palavra não é um palindromo")
 
